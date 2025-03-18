cf24945c9994a6b1fb6396e3c69eb0e405d6340f
JustGithubProject <Kropivadanil@gmail.com>
2025-03-18 16:06:59 +0200

    ✨ Defined config variables for database

---
+++ app/config.py
@@ -0,0 +1,12 @@
+import os
+
+from dotenv import load_dotenv
+
+
+# Parsing and loading .env file
+load_dotenv()
+
+
+SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
--- app/main.py
+++ app/main.py
@@ -1,11 +1,25 @@
 """ File that represents entry point """
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 from routes import user_routes
 
+from config import (
+    SQLALCHEMY_DATABASE_URI,
+    SQLALCHEMY_TRACK_MODIFICATIONS
+)
 
+
+# Creating an application object and registering user routes
 app = Flask(__name__)
 app.register_blueprint(user_routes)
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = SQLALCHEMY_TRACK_MODIFICATIONS
+
+
+db = SQLAlchemy(app)
 
 
 if __name__ == '__main__':
