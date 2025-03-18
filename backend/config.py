47a2ebeaf46a4a6ed91508f15a97b1f49976d7f1
JustGithubProject <Kropivadanil@gmail.com>
2025-03-18 18:46:11 +0200

    ✨ Containerize app with Docker

--- backend/config.py
+++ backend/config.py
@@ -1,11 +1,9 @@
 import os
-
 from dotenv import load_dotenv
 
 
 # Parsing and loading .env file
 load_dotenv()
-
 
 SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,8 @@
 """ File that represents entry point """
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 
 from routes import user_routes
-
+from models import db
 from config import (
     SQLALCHEMY_DATABASE_URI,
     SQLALCHEMY_TRACK_MODIFICATIONS
@@ -15,12 +14,17 @@ app = Flask(__name__)
 app.register_blueprint(user_routes)
 
 
+# Set up config variables
 app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = SQLALCHEMY_TRACK_MODIFICATIONS
 
 
-db = SQLAlchemy(app)
+db.init_app(app)
+
+# Creating tables
+with app.app_context():
+    db.create_all()
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
